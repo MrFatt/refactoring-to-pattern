ffade376b8baf88c5a6acb0625f1e05bc81707f6
Yingjian Li <yingjian.li@rea-group.com>
2019-08-10 15:37:03 +0800

    move duration to strategy

--- src/main/java/CapitalStrategy.java
+++ src/main/java/CapitalStrategy.java
@@ -1,4 +1,11 @@
+import java.time.LocalDate;
+import java.util.Iterator;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
 public class CapitalStrategy {
+
+    private static final long DAYS_PER_YEAR = 365;
 
     public double capital(Loan loan) {
         if (loan.expiry == null && loan.maturity != null)
@@ -16,11 +23,39 @@ public class CapitalStrategy {
         return 0.0;
     }
 
+    public double duration(Loan loan) {
+        if (loan.expiry == null && loan.maturity != null)
+            return weightedAverageDuration(loan);
+        else if (loan.expiry != null && loan.maturity == null)
+            return yearsTo(loan.expiry, loan);
+        return 0.0;
+    }
+
     private double riskFactorFor(Loan loan) {
         return RiskFactor.getFactors().forRating(loan.riskRating);
     }
 
     private double unusedRiskFactorFor(Loan loan) {
         return UnusedRiskFactors.getFactors().forRating(loan.riskRating);
+    }
+
+    private double weightedAverageDuration(Loan loan) {
+        double duration = 0.0;
+        double weightedAverage = 0.0;
+        double sumOfPayments = 0.0;
+        Iterator loanPayments = loan.payments.iterator();
+        while (loanPayments.hasNext()) {
+            Payment payment = (Payment) loanPayments.next();
+            sumOfPayments += payment.amount;
+            weightedAverage += yearsTo(payment.date, loan) * payment.amount;
+        }
+        if (loan.commitment != 0.0)
+            duration = weightedAverage / sumOfPayments;
+        return duration;
+    }
+
+    private double yearsTo(LocalDate endDate, Loan loan) {
+        LocalDate beginDate = (loan.today == null ? loan.start : loan.today);
+        return DAYS.between(beginDate, endDate) / DAYS_PER_YEAR;
     }
 }
--- src/main/java/Loan.java
+++ src/main/java/Loan.java
@@ -1,9 +1,6 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Loan {
 
@@ -16,12 +13,13 @@ public class Loan {
     LocalDate today;
     LocalDate start;
 
-    private long MILLIS_PER_DAY = 86400000;
-    private long DAYS_PER_YEAR = 365;
     double riskRating;
     double unusedPercentage;
 
-    public Loan(LocalDate expiry, LocalDate maturity, double commitment, LocalDate start, double riskRating) {
+    private CapitalStrategy capitalStrategy;
+
+    public Loan(LocalDate expiry, LocalDate maturity, double commitment, LocalDate start, double riskRating, CapitalStrategy capitalStrategy) {
+        this.capitalStrategy = capitalStrategy;
         this.expiry = expiry;
         this.maturity = maturity;
         this.commitment = commitment;
@@ -41,16 +39,16 @@ public class Loan {
     }
 
     public static Loan newTermLoan(double commitment, LocalDate start, LocalDate maturity, int riskRating) {
-        return new Loan(null, maturity, commitment, start, riskRating);
+        return new Loan(null, maturity, commitment, start, riskRating, new CapitalStrategy());
     }
 
     public static Loan newRevolver(double commitment, LocalDate start, LocalDate expiry, int riskRating) {
-        return new Loan(expiry, null, commitment, start, riskRating);
+        return new Loan(expiry, null, commitment, start, riskRating, new CapitalStrategy());
     }
 
     public static Loan newAdvisedLine(double commitment, LocalDate start, LocalDate expiry, int riskRating) {
         if (riskRating > 3) return null;
-        Loan advisedLine = new Loan(expiry, null, commitment, start, riskRating);
+        Loan advisedLine = new Loan(expiry, null, commitment, start, riskRating, new CapitalStrategy());
         advisedLine.setUnusedPercentage(0.1);
         return advisedLine;
     }
@@ -62,17 +60,13 @@ public class Loan {
 
     // 资本
     public double capital() {
-        return new CapitalStrategy().capital(this);
+        return capitalStrategy.capital(this);
     }
 
 
     //期限
     public double duration() {
-        if (expiry == null && maturity != null)
-            return weightedAverageDuration();
-        else if (expiry != null && maturity == null)
-            return yearsTo(expiry);
-        return 0.0;
+        return capitalStrategy.duration(this);
     }
 
 
@@ -83,34 +77,5 @@ public class Loan {
 
     double unusedRiskAmount() {
         return (commitment - outstanding);
-    }
-
-
-    private double weightedAverageDuration() {
-        double duration = 0.0;
-        double weightedAverage = 0.0;
-        double sumOfPayments = 0.0;
-        Iterator loanPayments = payments.iterator();
-        while (loanPayments.hasNext()) {
-            Payment payment = (Payment) loanPayments.next();
-            sumOfPayments += payment.amount;
-            weightedAverage += yearsTo(payment.date) * payment.amount;
-        }
-        if (commitment != 0.0)
-            duration = weightedAverage / sumOfPayments;
-        return duration;
-    }
-
-    private double yearsTo(LocalDate endDate) {
-        LocalDate beginDate = (today == null ? start : today);
-        return DAYS.between(beginDate, endDate) / DAYS_PER_YEAR;
-    }
-
-    private double riskFactor() {
-        return RiskFactor.getFactors().forRating(riskRating);
-    }
-
-    private double unusedRiskFactor() {
-        return UnusedRiskFactors.getFactors().forRating(riskRating);
     }
 }
